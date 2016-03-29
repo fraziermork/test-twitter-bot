bc78535308f9b5d125de489eb6740a56162c40c9
Lex Myers <lex.myers@gmail.com>
2016-03-29 16:42:11 -0700

    more goodies in searchStream()

--- server.js
+++ server.js
@@ -28,12 +28,23 @@ function findMentionsStream() {
 }
 
 function searchStream(searchString) {
-  var stream = t.stream('statuses/filter', { track: searchString});
+  var stream = t.stream('statuses/filter', { track: searchString });
   stream.on('tweet', function(tweet) {
     console.log(tweet.user.name);
     console.log(tweet.source);
     console.log(tweet.created_at);
     console.log(tweet.text);
+    console.log('coords: ' + tweet.coordinates);
+    console.log('mentions:');
+    tweet.entities.user_mentions.forEach((cur) => {
+      console.log('@' + cur.screen_name + ' ' + cur.name);
+    });
+    console.log('hashtags:');
+    tweet.entities.hashtags.forEach((cur) => {
+      console.log(cur.text);
+    });
+    // console.log('retweeted: ' + tweet.retweet_count);
+    // console.log('favorites: '+ tweet.favorite_count);
     console.log('**********');
   });
 }
