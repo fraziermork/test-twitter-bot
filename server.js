c1a0982f0281dec054041fd2c43dad5baab0e747
Lex Myers <lex.myers@gmail.com>
2016-03-30 16:35:19 -0700

    moved constructors out to constStream.js

--- server.js
+++ server.js
@@ -5,33 +5,6 @@ let keys      = require(__dirname + '/config/config.js');
 var t         = new Twit(keys);
 var lib       = {};
 require('./lib')(t, lib);
-
-var StreamOpts = function(searchString, callback) {
-  if(searchString.indexOf(',') >= 0 || searchString.indexOf(' ') >= 0) {
-    this.searchString = searchString.replace(/\,\ +/, ',', 'g');
-    this.type = 'text';
-  } else {
-    if (searchString.charAt(0) === '@') {
-      this.searchString = searchString;
-      this.type = 'mention';
-    } else if (searchString.charAt(0) === '#') {
-      this.searchString = searchString;
-      this.type = 'hashtag';
-    } else {
-      this.searchString = searchString;
-      this.type = 'text';
-    }
-  }
-
-  console.log(this.searchString);
-  this.callback = callback;
-};
-
-StreamOpts.optsArray = [];
-
-StreamOpts.addStreamOpts = function(searchString, type, callback) {
-  this.optsArray.push(new StreamOpts(searchString, type, callback));
-};
-
+let StreamOpts = lib.StreamOpts;
 
 require('./test/stream-test-inf')(t, lib, StreamOpts);
