b5793c9509650b5d03d7a7d34321203ac3a08fcb
Lex Myers <lex.myers@gmail.com>
2016-03-29 21:24:44 -0700

    moved functions from server.js to /lib

--- server.js
+++ server.js
@@ -3,76 +3,81 @@
 let Twit      = require('twit');
 let keys      = require(__dirname + '/config/config.js');
 var t         = new Twit(keys);
+var lib       = {};
 
+require('./lib')(t, lib);
 
-function saySomething (){
-  let today = new Date();
-  t.post('statuses/update', {status: 'It is ' + today.toISOString()}, (err, data, response) => {
-    console.log('Err is', err);
-    console.log('response is ', response);
-    console.log('data is ', data);
-  });
-}
-
-function findMentions() {
-  t.get('statuses/mentions_timeline', { }, function(err, data, response) {
-    console.log(data);
-  });
-}
-
-function findMentionsStream() {
-  var stream = t.stream('statuses/filter', { track: 'doceydraxton'});
-  stream.on('tweet', function(tweet) {
-    console.log(tweet);
-  });
-}
-
-function searchStream(searchString) {
-  var stream = t.stream('statuses/filter', { track: searchString });
-  stream.on('tweet', function(tweet) {
-    console.log(tweet.user.name);
-    console.log(tweet.source);
-    console.log(tweet.created_at);
-    console.log(tweet.text);
-    console.log('coords: ' + tweet.coordinates);
-    console.log('mentions:');
-    tweet.entities.user_mentions.forEach((cur) => {
-      console.log('@' + cur.screen_name + ' ' + cur.name);
-    });
-    console.log('hashtags:');
-    tweet.entities.hashtags.forEach((cur) => {
-      console.log(cur.text);
-    });
-    // console.log('retweeted: ' + tweet.retweet_count);
-    // console.log('favorites: '+ tweet.favorite_count);
-    console.log('**********');
-  });
-}
-
-function getUserInfo() {
-  t.get('account/verify_credentials', function(err, data, response) {
-    if(err) {
-      return console.error(err);
-    }
-    console.log('HERE BE data', data);
-    console.log('HERE BE response', response);
-  });
-}
-
-function getLatestFeed() {
-  t.get('account/verify_credentials', function(err, data, response) {
-    if(err) {
-      return console.error(err);
-    }
-    var myScreenName = data.screen_name;
-    var myId = data.id_str;
-    console.log('myScreenName is ' + myScreenName);
-    console.log('myId is ' + myId);
-  });
-  t.get('statuses/user_timeline', { user_id: MyId }, function(err, data, response) {
-
-  })
-}
+// function saySomething (){
+//   let today = new Date();
+//   t.post('statuses/update', {status: 'It is ' + today.toISOString()}, (err, data, response) => {
+//     console.log('Err is', err);
+//     console.log('response is ', response);
+//     console.log('data is ', data);
+//   });
+// }
+//
+// function findMentions() {
+//   t.get('statuses/mentions_timeline', { }, function(err, data, response) {
+//     console.log(data);
+//   });
+// }
+//
+// function findMentionsStream() {
+//   var stream = t.stream('statuses/filter', { track: 'doceydraxton'});
+//   stream.on('tweet', function(tweet) {
+//     console.log(tweet);
+//   });
+// }
+//
+// function searchStream(searchString, callback) {
+//   var stream = t.stream('statuses/filter', { track: searchString });
+//   stream.on('tweet', function(tweet) {
+//     console.log(tweet.user.name);
+//     console.log(tweet.source);
+//     console.log(tweet.created_at);
+//     console.log(tweet.text);
+//     console.log('coords: ' + tweet.coordinates);
+//     console.log('mentions:');
+//     tweet.entities.user_mentions.forEach((cur) => {
+//       console.log('@' + cur.screen_name + ' ' + cur.name);
+//     });
+//     console.log('hashtags:');
+//     tweet.entities.hashtags.forEach((cur) => {
+//       console.log(cur.text);
+//     });
+//     // console.log('retweeted: ' + tweet.retweet_count);
+//     // console.log('favorites: '+ tweet.favorite_count);
+//     console.log('**********');
+//   });
+// }
+//
+//
+//
+//
+// function getUserInfo() {
+//   t.get('account/verify_credentials', function(err, data, response) {
+//     if(err) {
+//       return console.error(err);
+//     }
+//     console.log('HERE BE data', data);
+//     console.log('HERE BE response', response);
+//   });
+// }
+//
+// function getLatestFeed() {
+//   t.get('account/verify_credentials', function(err, data, response) {
+//     if(err) {
+//       return console.error(err);
+//     }
+//     var myScreenName = data.screen_name;
+//     var myId = data.id_str;
+//     console.log('myScreenName is ' + myScreenName);
+//     console.log('myId is ' + myId);
+//   });
+//   t.get('statuses/user_timeline', { user_id: MyId }, function(err, data, response) {
+//
+//   })
+// }
 
 
 console.log('about to make request');
@@ -90,7 +95,7 @@ console.log('about to make request');
 try {
   console.log('Request made to twitter');
   // saySomething();
-  searchStream('beatles');
+  lib.searchStream('beatles');
 } catch (err) {
   console.log('Error occured:', err);
 }
